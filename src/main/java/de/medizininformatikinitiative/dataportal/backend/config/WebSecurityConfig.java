2a567b5447de85a77be226647a12e7beaf11da0a
Hannes Pieper <hannes.pieper@uk-erlangen.de>
2026-09-23 16:38:27 +0200

    chore: Add BZKF specific Additions

--- src/main/java/de/medizininformatikinitiative/dataportal/backend/config/WebSecurityConfig.java
+++ src/main/java/de/medizininformatikinitiative/dataportal/backend/config/WebSecurityConfig.java
@@ -54,7 +54,7 @@ public class WebSecurityConfig {
   public static final String PATH_PROFILE = "/profile";
   public static final String PATH_SWAGGER_UI = "/swagger-ui/**";
   public static final String PATH_SWAGGER_CONFIG = "/v3/api-docs/**";
-  public static final String PATH_SETTINGS = "/.settings";
+  public static final String PATH_SETTINGS = "/backend/.settings";
   @Value("${app.keycloakAllowedRole}")
   private String keycloakAllowedRole;
 
